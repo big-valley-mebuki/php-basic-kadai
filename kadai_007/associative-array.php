03a1c687f83efbf8d5e318b5a1773343200a79ee
big-valley-mebuki <166532852+big-valley-mebuki@users.noreply.github.com>
2024-07-17 21:44:49 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>連想配列</title>
+</head>
+<body>
+    <?php
+    //連想配列に値を代入する
+    $vegetables_data=['name'=>'onion','price'=>'200','weight'=>'160'];
+
+    //連想配列の値を出力する
+    print_r($vegetables_data);
+    ?>
+
+    
+</body>
+</html>
