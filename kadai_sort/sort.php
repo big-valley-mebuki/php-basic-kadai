813e0e5f0f3f7b0d6a8bae3ebc9b23a9268b6542
big-valley-mebuki <166532852+big-valley-mebuki@users.noreply.github.com>
2024-07-24 22:50:49 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP基礎編</title>
+</head>
+<body>
+
+<p>
+    <?php
+    // ソートする配列を宣言
+    $nums = [15, 4, 18, 23, 10];
+    function sort_2way($array, $order) {
+    if ($order===true) {
+        // 昇順を表示する
+        echo "昇順にソートします。<br>";
+        sort($array);
+        foreach ($array as $num) {
+            echo "{$num}<br>";
+        }
+    } else {
+        // 降順を教示する
+        echo "降順にソートします。<br>";
+        rsort($array);
+        foreach ($array as $num) {
+            echo "{$num}<br>";
+        }
+    }
+}
+
+sort_2way($nums, true);
+sort_2way($nums, false);
+?>
+    </p>
+    
+</body>
+</html>
